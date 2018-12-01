555ef69e500b32f08abaea5bcd0cabe62272a9e9
Barend <barend.poot@gmail.com>
2018-12-01 13:17:14 +0100

    init

---
+++ ReadME.md
@@ -0,0 +1,1 @@
+# Repo for the Advent code challenge
---
+++ day1/problem1.py
@@ -0,0 +1,15 @@
+def main():
+    freq = 0
+
+    with open("freq.txt", "r+") as file:
+        content = file.readlines()
+        for line in content:
+            if line[0] == '+':
+                freq = freq + int(line[1:])
+            else:
+                freq = freq - int(line[1:])
+        print(freq)
+
+
+if __name__ == "__main__":
+    main()
