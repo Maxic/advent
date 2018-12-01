edf019099759d7cb3ebba98e84f289e11906b241
Barend <barend.poot@gmail.com>
2018-12-01 14:14:31 +0100

    completed problem 1 and 2

--- day1/problem2.py
+++ day1/problem2.py
@@ -1,13 +1,25 @@
+import itertools
+
+
 def main():
     freq = 0
+    freq_set = set()
 
     with open("freq.txt", "r+") as file:
-        content = file.readlines()
-        for line in content:
-            if line[0] == '+':
+        content = itertools.cycle(file.readlines())
+        while True:
+            freq_set_length = freq_set.__len__()
+            freq_set.add(freq)
+            line = next(content)
+
+            if freq_set_length == freq_set.__len__():
+                break
+
+            if line[:1] == '+':
                 freq = freq + int(line[1:])
             else:
                 freq = freq - int(line[1:])
+
         print(freq)
 
 
