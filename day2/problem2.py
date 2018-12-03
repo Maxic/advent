1fac27cee10f59b1f6a47c48f6df527fde5e5db7
Barend <barend.poot@gmail.com>
2018-12-03 08:18:06 +0100

    day2 problem2

--- day2/problem2.py
+++ day2/problem2.py
@@ -1,6 +1,27 @@
 def main():
     with open("input.txt", "r+") as file:
         box_ids = file.readlines()
+    counter = 0
+    for box_id in box_ids:
+        counter += 1
+        for i in range(counter, box_ids.__len__()):
+            if box_id_differs_by_single_character(box_id, box_ids[i]):
+                print('Found one!\nBase_id:  ' + box_id + 'Match_id: ' + box_ids[i])
+
+
+# returns True if the boxId differs by exactly one character
+def box_id_differs_by_single_character(box_id1, box_id2):
+    diff_count = 0
+    for i in range(0,box_id1.__len__()):
+        if box_id1[i] != box_id2[i]:
+            diff_count += 1
+            if diff_count > 1:
+                return False
+
+    if diff_count == 1:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
