dc0ed8c5dd6fa4309a836200987fc2a9213c8a46
Barend <barend.poot@gmail.com>
2018-12-04 08:46:29 +0100

    problems for day3

--- day3/problem1.py
+++ day3/problem1.py
@@ -1,6 +1,61 @@
+import re
+
+
 def main():
     with open("input.txt", "r+") as file:
-        coordinates = file.readlines()
+        claims = file.readlines()
+
+    coordinates_list = []
+    max_xpos = 0
+    max_ypos = 0
+    overlap_count = 0
+
+    for claim in claims:
+        # Parse claim
+        match = re.match(r"(.*)@ (.*),(.*): (.*)x(.*)", claim.rstrip())
+        xpos = int(match.group(2))
+        ypos = int(match.group(3))
+        width = int(match.group(4))
+        length = int(match.group(5))
+
+        # create coordinate list
+        coordinates_list.append(get_coordinates(xpos, ypos, width, length))
+
+        # get boundaries
+        if xpos > max_xpos:
+            max_xpos = xpos
+        if ypos > max_ypos:
+            max_ypos = ypos
+
+    # fill coordinates
+    grid = [[0 for j in range(0, 1000)] for i in range(0, 1000)]
+    for coordinates in coordinates_list:
+        for coordinate in coordinates:
+            if grid[coordinate[0]][coordinate[1]] == 0:
+                grid[coordinate[0]][coordinate[1]] = 1
+            else:
+                grid[coordinate[0]][coordinate[1]] = 8
+
+    # check for overlap
+    for list in grid:
+        for item in list:
+            if item == 8:
+                overlap_count += 1
+
+    print(overlap_count)
+
+
+def print_grid(grid):
+    for list in grid:
+        print(list)
+
+
+def get_coordinates(xpos, ypos, width, length):
+    coordinates = []
+    for i in range(ypos,ypos+length):
+        for j in range(xpos, xpos+width):
+            coordinates.append((j, i))
+    return coordinates
 
 
 if __name__ == "__main__":
--- day3/problem2.py
+++ day3/problem2.py
@@ -1,6 +1,65 @@
+import re
+
+
 def main():
     with open("input.txt", "r+") as file:
-        coordinates = file.readlines()
+        claims = file.readlines()
+
+    coordinates_list = []
+    max_xpos = 0
+    max_ypos = 0
+
+    for claim in claims:
+        # Parse claim
+        match = re.match(r"(.*)@ (.*),(.*): (.*)x(.*)", claim.rstrip())
+        xpos = int(match.group(2))
+        ypos = int(match.group(3))
+        width = int(match.group(4))
+        length = int(match.group(5))
+
+        # create coordinate list
+        coordinates_list.append(get_coordinates(xpos, ypos, width, length))
+
+        # get boundaries
+        if xpos > max_xpos:
+            max_xpos = xpos
+        if ypos > max_ypos:
+            max_ypos = ypos
+
+    coordinates_id = 0
+    grid = [[0 for j in range(0, 1000)] for i in range(0, 1000)]
+
+    # fill coordinates
+    for coordinates in coordinates_list:
+        for coordinate in coordinates:
+            if grid[coordinate[0]][coordinate[1]] == 0:
+                grid[coordinate[0]][coordinate[1]] = 1
+            else:
+                grid[coordinate[0]][coordinate[1]] = 8
+
+    # check grid again for overlap
+    for coordinates in coordinates_list:
+        coordinates_id += 1
+        coordinates_overlap = False
+        for coordinate in coordinates:
+            if grid[coordinate[0]][coordinate[1]] == 8:
+                coordinates_overlap = True
+        if not coordinates_overlap:
+            print(coordinates_id)
+            break
+
+
+def print_grid(grid):
+    for list in grid:
+        print(list)
+
+
+def get_coordinates(xpos, ypos, width, length):
+    coordinates = []
+    for i in range(ypos,ypos+length):
+        for j in range(xpos, xpos+width):
+            coordinates.append((j, i))
+    return coordinates
 
 
 if __name__ == "__main__":
